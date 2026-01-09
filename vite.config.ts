5cf35c8f251b23781c5e7a9d92fba789450f7604
Trung <trung@example.com>
2026-01-09 15:59:16 +0700

    Fix syntax error in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
-  return {`n    base: "/studiopro-web/",
+  return {
+    base: "/studiopro-web/",
     server: {
       port: 3000,
       host: '0.0.0.0',
@@ -25,4 +26,3 @@ export default defineConfig(({ mode }) => {
     }
   };
 });
-
